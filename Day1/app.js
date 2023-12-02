7c4afbbe99612f61a215b1844acdff9b793e2e4e
Miha Bastašič <bastasic.miha@gmail.com>
2023-12-02 01:16:03 +0100

    Day1 part 2 not there yet!

--- Day1/app.js
+++ Day1/app.js
@@ -15,15 +15,41 @@ function loadHandler(event) {
 
 function start(inputFile) {
   let result = 0;
+  const values = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
   arrayCSV = inputFile.trim().split("\n");
-  for (const iterator of arrayCSV) {
+  for (let iterator of arrayCSV) {
     let newArray = [];
-    [...iterator].forEach((element) => {
-      if (!isNaN(element)) {
-        newArray.push(element);
+    //if first and last char is number, then write it in result
+    if (
+      Number.isInteger(parseInt(iterator[0])) &&
+      Number.isInteger(parseInt(iterator[iterator.length - 1]))
+    ) {
+      console.log(iterator, "začetek in konec je cifra");
+      result = parseInt(iterator[0] + iterator[iterator.length - 1]) + result;
+    } else {
+      // else replace words with numbers
+      console.log(iterator, "iterator pred replace");
+      for (const key in values) {
+        iterator = iterator.replaceAll(key, values[key]);
       }
-    });
-    result = parseInt(newArray[0] + newArray[newArray.length - 1]) + result;
+      console.log(iterator);
+      [...iterator].forEach((element) => {
+        if (!isNaN(element)) {
+          newArray.push(element);
+        }
+      });
+      result = parseInt(newArray[0] + newArray[newArray.length - 1]) + result;
+    }
   }
 
   console.log(`Sum of number is: ${result}`);
