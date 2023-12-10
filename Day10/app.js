ef19c66ba3ceaa406181126b6136416bfc2c2fc9
Miha Bastašič <bastasic.miha@gmail.com>
2023-12-10 23:11:52 +0100

    day10

--- Day10/app.js
+++ Day10/app.js
@@ -14,12 +14,9 @@ function loadHandler(event) {
 }
 
 function start(inputFile) {
-  console.log(
-    inputFile.split("\n").map((line) => line.split(" ").map((e) => parseInt(e)))
-  );
-  let myInput = inputFile
-    .split("\n")
-    .map((line) => line.split(" ").map((e) => parseInt(e)));
+  console.log(inputFile.split("\n").map((line) => line.split(""))[41][91]);
+  let myInput = inputFile.split("\n").map((line) => line.split(""));
+
   let result = [];
   let done = false;
   let middArr = [];
@@ -47,36 +44,4 @@ function start(inputFile) {
   });
 
   console.log(result.reduce((a, b) => a + b));
-}
-function allEqual(arr) {
-  return arr.every((v) => v === arr[0]);
-}
-function reduceArray(arr) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index++) {
-    if (index + 1 < arr.length) {
-      newArr.push(arr[index + 1] - arr[index]);
-    }
-  }
-  return newArr;
-}
-function getTheResult(arr) {
-  let adder = 0;
-  arr.reverse();
-  adder = arr[0].at(-1);
-  for (let index = 1; index < arr.length; index++) {
-    arr[index][arr[index].length - 1] = arr[index].at(-1) + adder;
-    adder = arr[index].at(-1);
-  }
-  return arr.at(-1).at(-1);
-}
-function getTheResult2(arr) {
-  let sub = 0;
-  arr.reverse();
-  sub = arr[0][0];
-  for (let index = 1; index < arr.length; index++) {
-    arr[index][0] = arr[index][0] - sub;
-    sub = arr[index][0];
-  }
-  return arr.at(-1)[0];
 }
